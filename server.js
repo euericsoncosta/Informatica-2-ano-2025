fcd4ae93065b644e3d6cebe65625c5818a29c8da
euericsoncosta <costaericson@hotmail.com>
2025-05-15 09:34:51 -0300

    dotenv

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from './index.js';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+// Define a porta do servidor, usando a variável de ambiente PORT ou 3000 como padrão
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
-
-// const express = requ
